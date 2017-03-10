2d7daefec1f134f5586db75ae5082e396f547271
nickpons666 <miguel.pons@outlook.com>
2017-03-10 14:09:43 -0600

    Inicio

---
+++ app/src/main/java/net/ddns/clan_destiny/clan_desttiny/Bienvenida.java
@@ -0,0 +1,14 @@
+package net.ddns.clan_destiny.clan_desttiny;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+
+    }
+}
